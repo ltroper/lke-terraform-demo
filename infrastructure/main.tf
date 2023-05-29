8d51f56209c4dda67304d675abd2de8342c65b22
Leon Troper <ltroper@gmail.com>
2023-05-29 13:01:11 -0600

    removed writing kubeconfig

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -30,15 +30,15 @@ resource "linode_lke_cluster" "CBC" {
 }
 
 
-locals {
-  kubeconfig = base64decode(linode_lke_cluster.CBC.kubeconfig)
-}
+# locals {
+#   kubeconfig = base64decode(linode_lke_cluster.CBC.kubeconfig)
+# }
 
-resource "null_resource" "write_kubeconfig" {
-  provisioner "local-exec" {
-    command = "mkdir ~/.kube && echo '${local.kubeconfig}' > ~/.kube/cbcPOC"
-  }
-}
+# resource "null_resource" "write_kubeconfig" {
+#   provisioner "local-exec" {
+#     command = "mkdir ~/.kube && echo '${local.kubeconfig}' > ~/.kube/cbcPOC"
+#   }
+# }
 
 
 
